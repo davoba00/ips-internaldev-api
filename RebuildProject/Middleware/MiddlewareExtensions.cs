798a05e19ebc9c3e780450a3175a226f49939237
davoba00 <david.obadovic@ips-energy.com>
2025-11-25 10:16:24 +0100

    AA-39

---
+++ RebuildProject/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace RebuildProject.Middleware
+{
+    public static class MiddlewareExtensions
+    {
+        #region Public Methods
+
+        public static IApplicationBuilder UseApiLogging(this IApplicationBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.UseMiddleware<ApiLoggingMiddleware>();
+        }
+
+        public static IApplicationBuilder UseRequestMiddleware(this IApplicationBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.UseMiddleware<RequestMiddleware>();
+        }
+
+        #endregion
+    }
+}
